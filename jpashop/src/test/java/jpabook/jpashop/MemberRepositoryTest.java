5b9bf7b929b5388c1edb7eaac444f39c866d00e8
injin <injin33@naver.com>
2023-02-14 22:55:27 +0900

    도메인 개발

---
+++ jpashop/src/test/java/jpabook/jpashop/MemberRepositoryTest.java
@@ -0,0 +1,16 @@
+package jpabook.jpashop;
+
+import jpabook.jpashop.repository.MemberRepository;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MemberRepositoryTest {
+
+    @Autowired
+    MemberRepository memberRepository;
+
+}
